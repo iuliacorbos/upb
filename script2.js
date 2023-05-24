adece5e6478ba77a37cf65e9a6a28beefe9d5328
iuliacorbos <134000779+iuliacorbos@users.noreply.github.com>
2023-05-24 18:45:23 +0300

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,28 @@
+// Obține formularul din document
+var form = document.querySelector('form');
+
+// Ascultă evenimentul de submit al formularului
+form.addEventListener('submit', function(event) {
+  event.preventDefault(); // Previne trimiterea formularului
+
+  var totalIntrebari = 20;
+  var raspunsuriCorecte = 0;
+
+  // Parcurge întrebările și verifică răspunsurile
+  for (var i = 1; i <= totalIntrebari; i++) {
+    var numeIntrebare = 'q'+ i;
+    var raspunsCorect = 'ja'; // Răspunsul corect este "da" pentru fiecare întrebare
+
+    var input = document.querySelector('input[name="' + numeIntrebare + '"]:checked');
+    if (input && input.value === raspunsCorect) {
+      raspunsuriCorecte++;
+    }
+  }
+
+  // Calculează procentajul de răspunsuri corecte
+  var procentajCorecte = (raspunsuriCorecte / totalIntrebari) * 100;
+
+  // Afișează rezultatul în pagina HTML
+  var rezultatContainer = document.getElementById('rezultat');
+  rezultatContainer.innerHTML = 'Antworten für den charismatischen Leader ' + raspunsuriCorecte + ' Fragen. Der Prozentsatz beträgt: ' + procentajCorecte.toFixed(2) + '%';
+});
